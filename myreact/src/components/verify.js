54f99efb469b48fcd6aae670a22158dcb67492cd
zigm <77489316+orgxtopaz@users.noreply.github.com>
2021-11-29 19:28:31 +0800

    Update verify.js

--- myreact/src/components/verify.js
+++ myreact/src/components/verify.js
@@ -64,7 +64,7 @@ const verifyaccount =(e)=>{
       userId:userId
   }
   
-  Axios.put("https://orgxtopaz-server.herokuapp.com/verify",data)
+  Axios.put("https://zigm-server.herokuapp.com/verify",data)
 
   .then(res =>{
 
